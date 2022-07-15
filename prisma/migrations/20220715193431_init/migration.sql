4c9caa10dc5f232c6040ee610101388a9ebab635
hugodutka <dutkahugo@gmail.com>
2022-07-15 19:39:47 +0000

    add prisma

---
+++ prisma/migrations/20220715193431_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from "@prisma/client"
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
+  const db = new prisma.PrismaClient();
+  const player = await db.player.create({ data: { username: "Hugo!" } });
+  console.log(player);
   res.status(200).json({ name: `John Doe` });
 }
