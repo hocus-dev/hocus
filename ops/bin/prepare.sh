b514930bf2ddbc771952844c3f55755845e9589a
hugodutka <dutkahugo@gmail.com>
2022-08-20 10:52:23 +0000

    better gitpod setup

--- ops/bin/prepare.sh
+++ ops/bin/prepare.sh
@@ -12,7 +12,4 @@ export DOCKER_BUILDKIT=1
 cd "$PROJECT_DIR""/ops/docker" &&
     docker-compose \
         -p rooms \
-        up -d
-# postgres needs a second to start up
-sleep 1
-cd "$PROJECT_DIR" && npx prisma migrate deploy
+        pull
