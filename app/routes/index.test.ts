fa15a75f2192c35d9d224dfb20d369089e22850a
hugodutka <dutkahugo@gmail.com>
2022-08-21 11:51:53 +0000

    working db tests

--- app/routes/index.test.ts
+++ app/routes/index.test.ts
@@ -1,5 +1,9 @@
-import { xd } from "~/test-setup";
+import { provideDb } from "~/test-utils/db";
 
-test.concurrent("index", () => {
-  console.log(xd);
-});
+test.concurrent(
+  "index",
+  provideDb(async (db) => {
+    await db.player.create({ data: { username: "hugodutka" } });
+    console.log(await db.player.findMany());
+  }),
+);
--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import remix from "@remix-run/express";
+// eslint-disable-next-line no-restricted-imports
 import { PrismaClient } from "@prisma/client";
 
 const app = express();
