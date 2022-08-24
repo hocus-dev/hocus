ecaf3864ff3a9a9a590876114bfc5e4bd388928f
hugodutka <dutkahugo@gmail.com>
2022-08-24 19:51:27 +0000

    remove plasmic

--- .gitpod.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM gitpod/workspace-full
-
-RUN yarn global add @plasmicapp/cli
-RUN echo 'export PATH="$PATH'":$(yarn global bin)\"" >> ~/.bashrc
--- ops/bin/plasmic-auth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -z "$PLASMIC_AUTH_FILE" ]; then
-  echo "PLASMIC_AUTH_FILE is not set"
-  exit 0
-fi
-
-echo "$PLASMIC_AUTH_FILE" > ~/.plasmic.auth
