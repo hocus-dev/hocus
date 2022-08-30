0605ac95cb7d7044ff92f18e1d0a4ea095382ea3
hugodutka <dutkahugo@gmail.com>
2022-08-30 22:16:12 +0000

    adjust for mobile

--- app/components/landing/terminal-screenshot.tsx
+++ app/components/landing/terminal-screenshot.tsx
@@ -1,7 +1,7 @@
 export const TerminalScreenshot = (): JSX.Element => {
   return (
     <>
-      <div className="flex justify-center text-center w-full mt-28">
+      <div className="flex justify-center text-center w-full mt-14 sm:mt-28">
         <div className="w-full relative">
           <div className="w-full flex justify-center">
             <div className="w-[50rem] relative">
@@ -12,17 +12,17 @@ export const TerminalScreenshot = (): JSX.Element => {
               />
               <img
                 src="/landing-page/left-leaf.png"
-                className="absolute top-0 -left-[12rem] w-96 h-96 -z-20"
+                className="absolute top-0 -left-[16rem] sm:-left-[12rem] w-96 h-96 -z-20"
                 alt="Left leaf"
               />
               <img
                 src="/landing-page/right-leaf.png"
-                className="absolute top-0 -right-[12rem] w-96 h-96 -z-20"
+                className="absolute top-0 -right-[16rem] sm:-right-[12rem] w-96 h-96 -z-20"
                 alt="Right leaf"
               />
             </div>
           </div>
-          <div className="absolute w-screen h-[25rem] top-[14rem] bg-gray-700 -z-10"></div>
+          <div className="absolute w-screen h-[25rem] top-[8rem] sm:top-[14rem] bg-gray-700 -z-10"></div>
         </div>
       </div>
     </>
