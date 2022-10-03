a22aac64021eb455475a81465e409ab234d55d5c
Hugo Dutka <contact@hugodutka.com>
2022-10-03 23:27:40 +0200

    add security cmd to trust-cert.sh

--- ops/bin/certs/trust-cert.sh
+++ ops/bin/certs/trust-cert.sh
@@ -3,3 +3,5 @@
 cp /etc/ssl/cert.pem.backup local.pem
 cat rootca.pem >> local.pem
 sudo cp local.pem /etc/ssl/cert.pem
+
+security add-trusted-cert -d -r trustRoot -k "$HOME/Library/Keychains/login.keychain" rootca.pem
