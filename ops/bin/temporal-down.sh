97ce784db476590e81192b3c687f1fe9b561d771
hugodutka <dutkahugo@gmail.com>
2022-10-23 18:26:07 +0000

    add temporal docker compose

---
+++ ops/bin/temporal-down.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+OPS_DIR=$(dirname $(dirname "$0"))
+
+cd "$OPS_DIR""/docker" && \
+docker-compose \
+    -p temporal \
+    down \
+    --remove-orphans
---
+++ ops/bin/temporal-up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+PROJECT_DIR=$(dirname $(dirname "$SCRIPTPATH"))
+
+# These env variables help with caching
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
+cd "$PROJECT_DIR""/ops/docker" &&
+    docker-compose \
+        --file temporal.docker-compose.yml \
+        -p temporal \
+        up -d
