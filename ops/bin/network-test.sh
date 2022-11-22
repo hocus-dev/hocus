ae8f6ed1cad4b7f1e572875ccd4f8e1720e10989
hugodutka <dutkahugo@gmail.com>
2022-11-22 15:32:28 +0000

    add a ns to test which acts like a hocus vm

--- ops/bin/network-test.sh
+++ ops/bin/network-test.sh
@@ -19,17 +19,37 @@ httptest() {
   fi
 }
 
+# Setup a namespace that will act as a vm spawned by the agent
+ip netns del hocusvm0 || true
+ip link delete veth-hocusvm0 || true
+ip link delete vpeer-hocusvm0 || true
+ip netns add hocusvm0
+ip netns exec vms ip link delete veth-hocusvm0 || true
+ip netns exec hocusvm0 ip link delete vpeer-hocusvm0 || true
+
+ip link add veth-hocusvm0 type veth peer name vpeer-hocusvm0
+ip link set veth-hocusvm0 netns vms
+ip link set vpeer-hocusvm0 netns hocusvm0
+ip netns exec vms ip addr add 10.231.0.4/31 dev veth-hocusvm0
+ip netns exec hocusvm0 ip addr add 10.231.0.5/16 dev vpeer-hocusvm0
+ip netns exec vms ip link set veth-hocusvm0 up
+ip netns exec hocusvm0 ip link set vpeer-hocusvm0 up
+
 killall python3 || true
 
 ip netns exec ssh python3 -m http.server -b 10.10.0.1 &
 ip netns exec vms python3 -m http.server -b 10.231.0.1 &
+ip netns exec hocusvm0 python3 -m http.server -b 10.231.0.5 &
 python3 -m http.server -b 0.0.0.0 &
 sleep 1
+
 
 httptest 10.10.0.1:8000
 httptest 10.231.0.1:8000
 httptest 127.0.0.1:8000
+httptest 10.231.0.5:8000 vms
 
 expect_fail httptest 10.10.0.0:8000 ssh
+expect_fail httptest 10.231.0.5:8000 ssh
 
 killall python3
--- ops/docker/resources/worker-dev-entrypoint.sh
+++ ops/docker/resources/worker-dev-entrypoint.sh
@@ -33,16 +33,18 @@ ip netns exec vms ip link set dev vpeer-ssh-vms up
 
 ip netns exec ssh service ssh start
 
-sysctl -w net.ipv4.conf.veth-ssh.proxy_arp=1
-sysctl -w net.ipv6.conf.veth-ssh.disable_ipv6=1
-
-iptables -A FORWARD -i veth-ssh -o veth-python -j ACCEPT
-iptables -A FORWARD -i veth-python -o veth-ssh -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A POSTROUTING -o veth-python -j MASQUERADE
-
+# Forward traffic from interface eth0 port 22 to the ssh service in the ssh ns
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 22 -j DNAT --to-destination 10.10.0.1:22
 iptables -t nat -A POSTROUTING -o veth-ssh -j MASQUERADE
 
 # Disable outgoing connections to the parent ns from the ssh ns, but enable incoming
 ip netns exec ssh iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 ip netns exec ssh iptables -A OUTPUT -j DROP -d 10.10.0.0/16
+
+
+# sysctl -w net.ipv4.conf.veth-ssh.proxy_arp=1
+# sysctl -w net.ipv6.conf.veth-ssh.disable_ipv6=1
+
+# iptables -A FORWARD -i veth-ssh -o veth-python -j ACCEPT
+# iptables -A FORWARD -i veth-python -o veth-ssh -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -t nat -A POSTROUTING -o veth-python -j MASQUERADE
