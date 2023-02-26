a39b0124c4f96f51434389081ddc5353e303aec1
Grzegorz Uriasz <gorbak25@gmail.com>
2023-02-26 18:45:29 +0000

    vscode-local uri

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -7,22 +7,32 @@ export function activate(context: vscode.ExtensionContext) {
   // For now don't hide anything in the UI
   vscode.commands.executeCommand("setContext", "hocus.insideHocusVM", true);
 
+  /* * Resources with the `file` scheme come from the] same extension host as the extension.
+   * * Resources with the `vscode-local` scheme come from an extension host running in the same place as the UI. */
+
   // TODO: Attach terminals to tasks
 
   vscode.window.registerUriHandler({
     handleUri(uri: vscode.Uri): vscode.ProviderResult<void> {
+      vscode.workspace.fs.readDirectory(vscode.Uri.parse("vscode-local:~/.ssh/")).then(console.log);
+
+      console.log(vscode.workspace.getConfiguration());
+
+      console.log(vscode.window.activeTextEditor?.document.uri);
+
       const p = new URLSearchParams(uri.query);
       const agentHostname = p.get("agent-hostname");
       const workspaceHostname = p.get("workspace-hostname");
       console.log(agentHostname);
       console.log(workspaceHostname);
+
       // TODO: parse the URL, connect to the remote machine and/or write the required ssh config
       vscode.window.showInformationMessage(`Hocus URI handler called: ${uri.toString()}`);
 
-      vscode.commands.executeCommand(
+      /*vscode.commands.executeCommand(
         "vscode.openFolder",
         vscode.Uri.parse("vscode-remote://ssh-remote+myubuntubox/home/x/projects/foo"),
-      );
+      );*/
     },
   });
 
