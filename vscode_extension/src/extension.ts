6a98aa9ef93703deb5bf0b3769e2153a7a95a725
Grzegorz Uriasz <gorbak25@gmail.com>
2023-02-27 02:43:24 +0100

    Manage ssh config via extension

--- vscode_extension/src/extension.ts
+++ vscode_extension/src/extension.ts
@@ -1,18 +1,78 @@
+import * as fs from "fs-extra";
+import * as os from "os";
+import * as path from "path";
 import * as vscode from "vscode";
 
-async function detectPlatform(context: vscode.ExtensionContext) {
-  console.log(context);
-  
+/* TODO: Windows support */
+async function ensureSupportedPlatform() {
+  const platform = os.platform();
+
+  if (platform !== "linux" && platform !== "darwin") {
+    vscode.window.showInformationMessage(`Unsupported platform ${platform} - TODO!`);
+    throw new Error("Nope");
+  }
 }
 
-export function activate(context: vscode.ExtensionContext) {
+/* TODO: Include statements are only supported from OpenSSH 7.3 released August 01, 2016 
+*  Inform the user if the SSH client is too old...
+*  Knowing debian users they surely will hit this case...
+*/
+async function ensureSshNewEnough() {
+  // ssh -V
+  // OpenSSH_9.2p1, OpenSSL 3.0.8 7 Feb 2023
+  return;
+}
+
+async function getSshConfigDir() {
+  // TODO: Windows support
+  return path.join(os.homedir(), ".ssh");
+}
+
+async function ensureSshDirsExists() {
+  const userSshDir = await getSshConfigDir();
+  const hocusSshDir = path.join(userSshDir, "hocus");
+  for (const dir of [userSshDir, hocusSshDir]) {
+    if (!await fs.exists(dir)) {
+      console.log(`Creating ${dir} dir`);
+      await fs.mkdir(dir);
+      await fs.chmod(dir, 0o700);
+    }
+  }
+}
+
+async function ensureSshConfigSetUp() {
+  await ensureSshDirsExists();
+  const userSshDir = await getSshConfigDir();
+  const sshUserConfigPath = path.join(userSshDir, "config");
+  const sshHocusConfigPath = path.join(userSshDir, "hocus", "config");
+  for (const [filePath, fileDefault] of [[sshUserConfigPath, "# This is the ssh client user configuration file. See\n# ssh_config(5) for more information.\n\n"], [sshHocusConfigPath, "# Don't edit this file - your changes will be overwritten!\n# This file is managed by the Hocus Vscode integration!\n"]] as const) {
+    if (!await fs.exists(filePath)) {
+      console.log(`Creating ssh config ${filePath}`);
+      await fs.createFile(filePath);
+      await fs.chmod(filePath, 0o600);
+      await fs.writeFile(filePath, fileDefault)
+    }
+  }
+
+  const config = await fs.readFile(sshUserConfigPath);
+  if (!config.includes("Include hocus/config")) {
+    console.log(`Installing integration into ssh config`);
+    // Now ensure the Hocus config path is included
+    fs.appendFile(sshUserConfigPath, "\n\n# Beginning of Hocus Vscode integration\nHost *.hocus.dev\n    Include hocus/config\n# End of Hocus Vscode integration\n")
+  }
+}
+
+export async function activate(context: vscode.ExtensionContext) {
   console.log("Hocus Activated");
-  detectPlatform(context);
+
+  await ensureSupportedPlatform();
+  await ensureSshNewEnough();
+  await ensureSshConfigSetUp();
+
   // TODO: Detect if we are inside a Hocus VM
   // TODO: The simplest way is to check for a JWT/OIDC token
   // For now don't hide anything in the UI
   vscode.commands.executeCommand("setContext", "hocus.insideHocusVM", true);
-  vscode.commands.getCommands().then((x) => x.forEach((y) => console.log(y)));
 
   /* * Resources with the `file` scheme come from the] same extension host as the extension.
    * * Resources with the `vscode-local` scheme come from an extension host running in the same place as the UI. */
