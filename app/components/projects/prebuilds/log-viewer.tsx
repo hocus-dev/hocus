61cf78b168afde22a9be9bc61cbdcedc1f2763e2
hugodutka <dutkahugo@gmail.com>
2023-03-03 12:07:36 +0000

    scroll log viewer up with an inline script

--- app/components/projects/prebuilds/log-viewer.tsx
+++ app/components/projects/prebuilds/log-viewer.tsx
@@ -10,11 +10,20 @@ export const LogViewer = (props: { text: string }) => {
   }, []);
 
   return (
-    <textarea
-      ref={textAreaRef}
-      value={props.text}
-      readOnly={true}
-      className="p-6 w-full h-full bg-gray-900 border-none resize-none text-sm"
-    ></textarea>
+    <>
+      <textarea
+        ref={textAreaRef}
+        id="log-viewer"
+        value={props.text}
+        readOnly={true}
+        className="p-6 w-full h-full bg-gray-900 border-none resize-none text-sm"
+      ></textarea>
+      {/* I'm doing it here too because useEffect lags visibly */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `const logViewer = document.getElementById("log-viewer"); logViewer.scrollTop = logViewer.scrollHeight;`,
+        }}
+      ></script>
+    </>
   );
 };
