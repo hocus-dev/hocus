eb757cd513676cb3546c15e0e1d0be8206ab6eed
Grzegorz Uriasz <gorbak25@gmail.com>
2023-03-08 23:46:39 +0000

    Workspace detection in extension

--- extensions/vscode_workspace/CHANGELOG.md
+++ extensions/vscode_workspace/CHANGELOG.md
@@ -1,9 +1,5 @@
 # Change Log
 
-All notable changes to the "hocus" extension will be documented in this file.
+## UNRELEASED
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-
-- Initial release
+- Release under MIT license
--- extensions/vscode_workspace/src/extension.ts
+++ extensions/vscode_workspace/src/extension.ts
@@ -1,7 +1,25 @@
+import * as fs from "fs-extra";
+import * as os from "os";
 import * as vscode from "vscode";
+
+const HOCUS_DIR = "/home/hocus/dev/.hocus/";
+
+export async function isHocusWorkspace(): Promise<boolean> {
+  // No need to probe if running in the web or locally
+  if (vscode.env.remoteName === void 0 || vscode.env.uiKind !== vscode.UIKind.Desktop) {
+    return false;
+  }
+
+  // Assume that if this magic folder exists and we're on linux then we're in a Hocus workspace
+  const platform = os.platform();
+  return platform === "linux" && await fs.exists(HOCUS_DIR);
+}
 
 export async function activate(context: vscode.ExtensionContext) {
   console.log("Hocus Workspace Activated");
+  const isWorkspace = await isHocusWorkspace();
+  console.log(isWorkspace);
+
 }
 
 export function deactivate() {
