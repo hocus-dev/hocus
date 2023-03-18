0bd3e8e690a07eb9af0c3b3b7403ed7b78ef3833
Grzegorz Uriasz <gorbak25@gmail.com>
2023-03-18 17:24:01 +0000

    Ensure the docker-compose deployment doesn't fail on a clean start

--- ops/docker/resources/keycloak-setup.sh
+++ ops/docker/resources/keycloak-setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 CREATE DATABASE keycloak;
-CREATE DATABASE temporal;
 EOSQL
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "keycloak" <<-EOSQL
 -- SQL_DB_DUMP_GOES_HERE
+EOSQL
+
+# It's in the end cause this acts as a check if this script finished :)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+CREATE DATABASE temporal;
 EOSQL
