a0e0b729722803e1e3ea432157659415cdf2c50b
Grzegorz Uriasz <gorbak25@gmail.com>
2023-03-22 15:31:15 +0100

    Also push latest tag

--- hocus-workspace/build-and-push.sh
+++ hocus-workspace/build-and-push.sh
@@ -4,4 +4,6 @@ export DOCKER_BUILDKIT=1
 
 TAG=$(date +"%d-%m-%Y")
 docker build --tag hocusdev/hocus-workspace:$TAG -f hocus.Dockerfile resources
+docker tag hocusdev/hocus-workspace:$TAG hocusdev/hocus-workspace:latest
 docker push hocusdev/hocus-workspace:$TAG
+docker push hocusdev/hocus-workspace:latest
