6b5708dffb9ba3d48ded0f7edbac009ea29ec9b6
Hugo Dutka <contact@hugodutka.com>
2023-03-27 16:24:37 +0000

    add the local-down script

---
+++ ops/bin/local-down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export DOCKER_BUILDKIT=1
+
+SCRIPT_DIR="$(dirname "$0")"
+export REPO_DIR="$(realpath "${SCRIPT_DIR}/../..")"
+export HOCUS_RESOURCES_DIR="$(realpath ${REPO_DIR}/../hocus-resources)"
+
+cd "$SCRIPT_DIR"
+"$REPO_DIR/ops/bin/local-cmd.sh" down
