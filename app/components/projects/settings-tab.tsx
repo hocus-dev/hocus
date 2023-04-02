701ea3ea9f6192115a0a6678c6e434b6901ff5df
Hugo Dutka <contact@hugodutka.com>
2023-04-02 22:36:23 +0000

    refactor input field props

--- app/components/projects/settings-tab.tsx
+++ app/components/projects/settings-tab.tsx
@@ -4,21 +4,25 @@ import { MAX_REPOSITORY_DRIVE_SIZE_MIB } from "~/constants.shared";
 
 import { TextInputAddonRight } from "../text-input-addon-right";
 
-function InputFieldComponent<T extends string | number>(props: {
-  title: string;
-  inputName: string;
-  initialValue: T;
-  value: T;
+function InputFieldComponent(props: {
+  title: React.ReactNode;
   unit: string;
-  size?: string;
-  onChange?: (value: T) => void;
+  inputExtraClassName?: string;
+  inputProps?: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >;
 }): JSX.Element {
   return (
-    <div className="flex flex-col justify-between">
+    <div className={`flex flex-col justify-end`}>
       <h3 className="text-sm text-gray-400 mb-2">{props.title}</h3>
       <TextInputAddonRight
-        inputExtraClassName={`max-w-[${props.size ?? "6rem"}]`}
-        value={props.value}
+        inputExtraClassName={props.inputExtraClassName ?? "max-w-[6rem]"}
+        inputProps={{
+          ...props.inputProps,
+          type: "number",
+          onChange: () => {},
+        }}
         addon={<span>{props.unit}</span>}
       />
     </div>
@@ -27,7 +31,7 @@ function InputFieldComponent<T extends string | number>(props: {
 
 const InputField = React.memo(InputFieldComponent);
 
-function SettingsTabComponent(_props: { maxPrebuildRamMib: number }): JSX.Element {
+function SettingsTabComponent(props: { maxPrebuildRamMib: number }): JSX.Element {
   return (
     <form>
       <div className="flex flex-col gap-4">
@@ -37,32 +41,38 @@ function SettingsTabComponent(_props: { maxPrebuildRamMib: number }): JSX.Elemen
           <div className="flex gap-4">
             <InputField
               title="Virtual CPU Cores"
-              inputName="prebuildVCPU"
-              initialValue={4}
-              value={4}
+              inputProps={{
+                name: "prebuildVCPU",
+                defaultValue: 4,
+                min: 1,
+              }}
               unit="VCPU"
+              inputExtraClassName="max-w-[5rem]"
             />
             <InputField
               title="RAM"
-              inputName="workspaceRam"
-              initialValue={4096}
-              value={4096}
+              inputProps={{
+                name: "workspaceRam",
+                defaultValue: 4096,
+                min: 1,
+              }}
               unit="MiB"
             />
             <InputField
               title="Max Root Filesystem Size"
-              inputName="workspaceRootSize"
-              initialValue={10000}
-              value={10000}
+              inputProps={{
+                name: "workspaceRootSize",
+                defaultValue: 10000,
+                min: 1,
+              }}
               unit="MiB"
-              size={"10rem"}
+              inputExtraClassName="max-w-[10rem]"
             />
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-end">
               <h3 className="text-sm text-gray-400 mb-2">Max Project Filesystem Size</h3>
               <TextInputAddonRight
                 inputExtraClassName="max-w-[10rem]"
-                disabled={true}
-                value={MAX_REPOSITORY_DRIVE_SIZE_MIB}
+                inputProps={{ disabled: true, value: MAX_REPOSITORY_DRIVE_SIZE_MIB }}
                 addon={<span>MiB</span>}
               />
             </div>
@@ -77,33 +87,42 @@ function SettingsTabComponent(_props: { maxPrebuildRamMib: number }): JSX.Elemen
           <div className="flex gap-4">
             <InputField
               title="Virtual CPU Cores"
-              inputName="prebuildVCPU"
-              initialValue={8}
-              value={8}
+              inputProps={{
+                name: "prebuildVCPU",
+                defaultValue: 8,
+                min: 1,
+              }}
               unit="VCPU"
+              inputExtraClassName="max-w-[5rem]"
             />
             <InputField
               title="RAM"
-              inputName="workspaceRam"
-              initialValue={16192}
-              value={16192}
+              inputProps={{
+                name: "workspaceRam",
+                defaultValue: 16192,
+                min: 1,
+              }}
               unit="MiB"
             />
             <InputField
               title="Max Root Filesystem Size"
-              inputName="workspaceRootSize"
-              initialValue={250000}
-              value={250000}
+              inputProps={{
+                name: "workspaceRootSize",
+                defaultValue: 250000,
+                min: 1,
+              }}
               unit="MiB"
-              size={"10rem"}
+              inputExtraClassName="max-w-[10rem]"
             />
             <InputField
               title="Max Root Project Size"
-              inputName="workspaceRootSize"
-              initialValue={250000}
-              value={250000}
+              inputProps={{
+                name: "workspaceRootSize",
+                defaultValue: 250000,
+                min: 1,
+              }}
               unit="MiB"
-              size={"10rem"}
+              inputExtraClassName="max-w-[10rem]"
             />
           </div>
           <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
--- app/components/text-input-addon-right.tsx
+++ app/components/text-input-addon-right.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 
-const TextInputAddonRightComponent = (
-  props: React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
-    addon: React.ReactNode;
-    helperText?: React.ReactNode;
-    inputExtraClassName?: string;
-  },
-): JSX.Element => {
+const TextInputAddonRightComponent = (props: {
+  inputProps: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >;
+  addon: React.ReactNode;
+  helperText?: React.ReactNode;
+  inputExtraClassName?: string;
+}): JSX.Element => {
   let inputClassName =
     "rounded-none rounded-l-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
   if (props.inputExtraClassName != null) {
     inputClassName += " " + props.inputExtraClassName;
   }
-  if (props.disabled) {
+  if (props.inputProps.disabled) {
     inputClassName += " cursor-not-allowed !text-gray-400";
   }
   return (
     <>
       <div className="flex">
-        <input type="text" className={inputClassName} {...props} />
+        <input type="text" className={inputClassName} {...props.inputProps} />
         <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-l-0 border-gray-300 rounded-r-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
           {props.addon}
         </span>
