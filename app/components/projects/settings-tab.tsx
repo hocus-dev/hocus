97a75c3cc3f59d8dcf3c604afcce6b5524a76fd6
Hugo Dutka <contact@hugodutka.com>
2023-04-10 12:25:09 +0000

    add the edited variable in settings tab

--- app/components/projects/settings-tab.tsx
+++ app/components/projects/settings-tab.tsx
@@ -6,25 +6,59 @@ import { MAX_REPOSITORY_DRIVE_SIZE_MIB } from "~/constants.shared";
 
 import { TextInputAddonRight } from "../text-input-addon-right";
 
-import type { valueof } from "~/types/utils";
+const VmSettingsFields = [
+  "maxPrebuildRamMib",
+  "maxPrebuildVCPUCount",
+  "maxWorkspaceRamMib",
+  "maxWorkspaceVCPUCount",
+  "maxWorkspaceProjectDriveSizeMib",
+  "maxWorkspaceRootDriveSizeMib",
+  "maxPrebuildRootDriveSizeMib",
+] as const;
+
+export type VmSettingsField = typeof VmSettingsFields[number];
+// enforce that VmSettingsField is a subset of Project fields
+const _typecheck: Any.Contains<VmSettingsField, keyof Project> = 1;
+
+export const VmSettingsField = Object.fromEntries(
+  VmSettingsFields.map((field) => [field, field]),
+) as { [key in VmSettingsField]: key };
+
+type InputContextType = {
+  initialValues: Record<VmSettingsField, number>;
+  setEdited: React.Dispatch<React.SetStateAction<Record<VmSettingsField, boolean>>>;
+  edited: Record<VmSettingsField, boolean>;
+};
+
+const InputContext = React.createContext<InputContextType>({} as any);
 
 function InputFieldComponent(props: {
   title: React.ReactNode;
   unit: string;
-  inputName: string;
-  initialValue: number;
+  inputName: VmSettingsField;
   inputExtraClassName?: string;
   inputProps?: React.DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   >;
 }): JSX.Element {
-  const [value, setValue] = React.useState(props.initialValue);
-  const onChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.currentTarget.validity.valid) {
-      setValue(Number(e.currentTarget.value));
-    }
-  }, []);
+  const context = React.useContext(InputContext);
+  const initialValue = context.initialValues[props.inputName];
+  const [value, setValue] = React.useState(initialValue);
+  const edited = value !== initialValue;
+  const onChange = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (!e.currentTarget.validity.valid) {
+        return;
+      }
+      const value = Number(e.currentTarget.value);
+      setValue(value);
+      if (edited !== context.edited[props.inputName]) {
+        context.setEdited((prev) => ({ ...prev, [props.inputName]: edited }));
+      }
+    },
+    [context, props.inputName, edited],
+  );
   return (
     <div className="flex flex-col justify-end">
       <h3 className="text-sm text-gray-400 mb-2">{props.title}</h3>
@@ -33,7 +67,7 @@ function InputFieldComponent(props: {
         inputProps={{
           ...props.inputProps,
           value,
-          name: props.inputName,
+          name: edited ? props.inputName : void 0,
           type: "number",
           onChange,
         }}
@@ -45,134 +79,120 @@ function InputFieldComponent(props: {
 
 const InputField = React.memo(InputFieldComponent);
 
-export type VmSettingsField = valueof<typeof VmSettingsField>;
-export const VmSettingsField = {
-  maxPrebuildRamMib: "maxPrebuildRamMib",
-  maxPrebuildVCPUCount: "maxPrebuildVCPUCount",
-  maxWorkspaceRamMib: "maxWorkspaceRamMib",
-  maxWorkspaceVCPUCount: "maxWorkspaceVCPUCount",
-  maxWorkspaceProjectDriveSizeMib: "maxWorkspaceProjectDriveSizeMib",
-  maxWorkspaceRootDriveSizeMib: "maxWorkspaceRootDriveSizeMib",
-  maxPrebuildRootDriveSizeMib: "maxPrebuildRootDriveSizeMib",
-} as const;
-
-type VmSettings = {
+function SettingsTabComponent(props: {
   [key in VmSettingsField]: number;
-};
-
-// enforce that VmSettingsField is a subset of Project fields
-const _typecheck: Any.Contains<VmSettingsField, keyof Project> = 1;
-
-function SettingsTabComponent(props: VmSettings): JSX.Element {
+}): JSX.Element {
   const vmSettings = props;
+  const [edited, setEdited] = React.useState(
+    Object.fromEntries(VmSettingsFields.map((field) => [field, false])) as Record<
+      VmSettingsField,
+      boolean
+    >,
+  );
 
   return (
-    <form>
-      <div className="flex flex-col gap-4">
-        <h1 className="font-bold text-xl mb-4">Project Settings</h1>
-        <div>
-          <h2 className="font-bold text-lg mb-2">Prebuild Limits</h2>
-          <div className="flex gap-4">
-            <InputField
-              title="Virtual CPU Cores"
-              inputProps={{
-                min: 1,
-              }}
-              unit="VCPU"
-              inputExtraClassName="max-w-[5rem]"
-              inputName={VmSettingsField.maxPrebuildVCPUCount}
-              initialValue={vmSettings.maxPrebuildVCPUCount}
-            />
-            <InputField
-              title="RAM"
-              inputProps={{
-                min: 1,
-              }}
-              unit="MiB"
-              inputName={VmSettingsField.maxPrebuildRamMib}
-              initialValue={vmSettings.maxPrebuildRamMib}
-            />
-            <InputField
-              title="Max Root Filesystem Size"
-              inputProps={{
-                min: 1,
-              }}
-              unit="MiB"
-              inputExtraClassName="max-w-[10rem]"
-              inputName={VmSettingsField.maxPrebuildRootDriveSizeMib}
-              initialValue={vmSettings.maxPrebuildRootDriveSizeMib}
-            />
-            <div className="flex flex-col justify-end">
-              <h3 className="text-sm text-gray-400 mb-2">Max Project Filesystem Size</h3>
-              <TextInputAddonRight
+    <InputContext.Provider value={{ initialValues: vmSettings, setEdited, edited }}>
+      <form>
+        <div className="flex flex-col gap-4">
+          <h1 className="font-bold text-xl mb-4">Project Settings</h1>
+          <div>
+            <h2 className="font-bold text-lg mb-2">Prebuild Limits</h2>
+            <div className="flex gap-4">
+              <InputField
+                title="Virtual CPU Cores"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="VCPU"
+                inputExtraClassName="max-w-[5rem]"
+                inputName={VmSettingsField.maxPrebuildVCPUCount}
+              />
+              <InputField
+                title="RAM"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="MiB"
+                inputName={VmSettingsField.maxPrebuildRamMib}
+              />
+              <InputField
+                title="Max Root Filesystem Size"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="MiB"
                 inputExtraClassName="max-w-[10rem]"
-                inputProps={{ disabled: true, value: MAX_REPOSITORY_DRIVE_SIZE_MIB }}
-                addon={<span>MiB</span>}
+                inputName={VmSettingsField.maxPrebuildRootDriveSizeMib}
+              />
+              <div className="flex flex-col justify-end">
+                <h3 className="text-sm text-gray-400 mb-2">Max Project Filesystem Size</h3>
+                <TextInputAddonRight
+                  inputExtraClassName="max-w-[10rem]"
+                  inputProps={{ disabled: true, value: MAX_REPOSITORY_DRIVE_SIZE_MIB }}
+                  addon={<span>MiB</span>}
+                />
+              </div>
+            </div>
+            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+              Resources available to a virtual machine running a prebuild. Increasing filesystem
+              size will lengthen prebuild times. You should change it if your workspace image is too
+              large.
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg mb-2 mt-4">Workspace Limits</h2>
+            <div className="flex gap-4">
+              <InputField
+                title="Virtual CPU Cores"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="VCPU"
+                inputExtraClassName="max-w-[5rem]"
+                inputName={VmSettingsField.maxWorkspaceVCPUCount}
+              />
+              <InputField
+                title="RAM"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="MiB"
+                inputName={VmSettingsField.maxWorkspaceRamMib}
+              />
+              <InputField
+                title="Max Root Filesystem Size"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="MiB"
+                inputExtraClassName="max-w-[10rem]"
+                inputName={VmSettingsField.maxWorkspaceRootDriveSizeMib}
+              />
+              <InputField
+                title="Max Root Project Size"
+                inputProps={{
+                  min: 1,
+                }}
+                unit="MiB"
+                inputExtraClassName="max-w-[10rem]"
+                inputName={VmSettingsField.maxWorkspaceProjectDriveSizeMib}
               />
             </div>
+            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
+              Resources available inside a workspace.
+            </p>
           </div>
-          <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-            Resources available to a virtual machine running a prebuild. Increasing filesystem size
-            will lengthen prebuild times. You should change it if your workspace image is too large.
-          </p>
-        </div>
-        <div>
-          <h2 className="font-bold text-lg mb-2 mt-4">Workspace Limits</h2>
-          <div className="flex gap-4">
-            <InputField
-              title="Virtual CPU Cores"
-              inputProps={{
-                min: 1,
-              }}
-              unit="VCPU"
-              inputExtraClassName="max-w-[5rem]"
-              inputName={VmSettingsField.maxWorkspaceVCPUCount}
-              initialValue={vmSettings.maxWorkspaceVCPUCount}
-            />
-            <InputField
-              title="RAM"
-              inputProps={{
-                min: 1,
-              }}
-              unit="MiB"
-              inputName={VmSettingsField.maxWorkspaceRamMib}
-              initialValue={vmSettings.maxWorkspaceRamMib}
-            />
-            <InputField
-              title="Max Root Filesystem Size"
-              inputProps={{
-                min: 1,
-              }}
-              unit="MiB"
-              inputExtraClassName="max-w-[10rem]"
-              inputName={VmSettingsField.maxWorkspaceRootDriveSizeMib}
-              initialValue={vmSettings.maxWorkspaceRootDriveSizeMib}
-            />
-            <InputField
-              title="Max Root Project Size"
-              inputProps={{
-                min: 1,
-              }}
-              unit="MiB"
-              inputExtraClassName="max-w-[10rem]"
-              inputName={VmSettingsField.maxWorkspaceProjectDriveSizeMib}
-              initialValue={vmSettings.maxWorkspaceProjectDriveSizeMib}
-            />
-          </div>
-          <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-            Resources available inside a workspace.
-          </p>
-        </div>
-        <div className="border-t border-gray-700"></div>
+          <div className="border-t border-gray-700"></div>
 
-        <div className="flex justify-end">
-          <Button className="w-fit" type="submit" color="success">
-            <i className="fa-solid fa-floppy-disk mr-2"></i>
-            <span>Save Changes</span>
-          </Button>
+          <div className="flex justify-end">
+            <Button className="w-fit" type="submit" color="success">
+              <i className="fa-solid fa-floppy-disk mr-2"></i>
+              <span>Save Changes</span>
+            </Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </InputContext.Provider>
   );
 }
 
