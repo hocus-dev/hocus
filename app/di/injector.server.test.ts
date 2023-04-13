07dd2b50521f38609daeab4486bd9b18b615ca71
Hugo Dutka <contact@hugodutka.com>
2023-04-13 12:50:59 +0000

    refactor custom injector

--- app/di/injector.server.test.ts
+++ app/di/injector.server.test.ts
@@ -1,14 +1,31 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+
 import { Injector } from "./injector.server";
 
-class Test1 {}
+const token = {
+  a: "a",
+  b: "b",
+  c: "c",
+} as const;
+
+class Test1 {
+  static inject = [token.a, token.b] as const;
+  constructor(public a: number, public b: number) {}
+}
 
 test("injector", () => {
-  const injector = new Injector<{ a: 1; b: 2; c: 3 }>([
-    { token: "a", provide: { value: 1 } },
-    { token: "b", provide: { factory: () => 2 } },
-    { token: "c", provide: { value: 3 } },
-  ]);
+  const providers = [
+    { token: token.a, provide: { value: 1 } },
+    { token: token.b, provide: { factory: () => 2 as const } },
+    { token: token.c, provide: { class: Test1 } },
+  ] as const;
+  const injector = new Injector(providers);
   expect(injector.resolve("a")).toBe(1);
   expect(injector.resolve("b")).toBe(2);
-  expect(injector.resolve("c")).toBe(3);
+  const c = injector.resolve("c");
+  expect(c instanceof Test1).toBe(true);
+  expect(c.a).toBe(1);
+  expect(c.b).toBe(2);
+  expect(() => injector.resolve("invalid")).toThrow();
 });
--- app/di/injector.server.ts
+++ app/di/injector.server.ts
@@ -1,5 +1,5 @@
 import { match } from "ts-pattern";
-import type { Union } from "ts-toolbelt";
+import type { Any } from "ts-toolbelt";
 
 import { InjectValidator } from "./inject.validator.server";
 
@@ -11,39 +11,63 @@ export const Scope = {
   Singleton: "singleton",
 } as const;
 
+export type Provide<V> =
+  | { value: V; class?: undefined; factory?: undefined }
+  | { class: new (...args: any[]) => V; value?: undefined; factory?: undefined }
+  | { factory: () => V; value?: undefined; class?: undefined };
+
 export type Provider<T extends string, V> = {
   token: T;
   scope?: Scope;
-  provide: { value: V } | { class: new (...args: any[]) => V } | { factory: () => V };
+  provide: Provide<V>;
 };
 
-type ProvidersArray<T> = T extends { [_1 in keyof T & string]: infer _2 }
-  ? Union.ListOf<valueof<{ [K in keyof T & string]: Provider<K, T[K]> }>>
+type GenericProviderMap<T> = T extends {
+  [key in keyof T]: Provider<infer _1, infer _2>;
+}
+  ? {
+      [K in keyof T & string as T[K]["token"]]: T[K]["provide"] extends Provide<infer V>
+        ? V
+        : never;
+    }
   : never;
 
 type Providers<T> = T extends { [_1 in keyof T]: infer _2 }
   ? { [K in keyof T]: () => T[K] }
   : never;
 
-type ResolveResult<T, K extends keyof T> = T extends { [_1 in keyof T & string]: infer _2 }
-  ? T[K]
+type ResolveResult<T, K extends keyof T> = T extends { [_1 in keyof T]: infer _2 }
+  ? Any.Equals<T[K], unknown> extends 0
+    ? T[K]
+    : never
   : never;
 
-export class Injector<T> {
-  private providers: Providers<T>;
+export class Injector<T, M extends GenericProviderMap<T>, P extends Providers<M>> {
+  private providers: P;
 
-  constructor(providersArg: ProvidersArray<T>) {
+  private getInject<Tn extends string, Val>(provider: Provider<Tn, Val>): string[] | undefined {
+    const provide = provider.provide;
+    let inject: unknown = void 0;
+    if (provide.class != null) {
+      inject = (provide.class as any).inject;
+    } else if (provide.factory != null) {
+      inject = (provide.factory as any).inject;
+    } else if (provide.value != null) {
+      inject = void 0;
+    }
+    const { success, value } = InjectValidator.SafeParse(inject);
+    if (!success) {
+      throw new Error(`Invalid inject value for provider ${provider.token}`);
+    }
+    return value;
+  }
+
+  constructor(providersArg: T) {
     const providers = providersArg as any as Provider<string, null>[];
     const local = {} as Record<string, () => any>;
 
     for (const provider of providers) {
-      const { success, value: inject } = InjectValidator.SafeParse(
-        (provider.provide as any)?.inject,
-      );
-      if (!success) {
-        throw new Error(`Invalid inject value for provider ${provider.token}`);
-      }
-
+      const inject = this.getInject(provider);
       const paramFns: (() => any)[] = [];
       if (inject != null) {
         for (const token of inject) {
@@ -80,7 +104,7 @@ export class Injector<T> {
     this.providers = local as any;
   }
 
-  resolve<K extends keyof T>(token: K): ResolveResult<T, K> {
+  resolve<K extends keyof M & string>(token: K): ResolveResult<M, K> {
     const provider = this.providers[token];
     if (provider == null) {
       throw new Error(`Missing provider for token ${String(token)}`);
