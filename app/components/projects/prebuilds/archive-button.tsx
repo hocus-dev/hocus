978d85738825862e2390d9797da302f35df6ef36
Hugo Dutka <contact@hugodutka.com>
2023-05-11 10:43:33 +0000

    add archive button to prebuild page

---
+++ app/components/projects/prebuilds/archive-button.tsx
@@ -0,0 +1,19 @@
+import { FormButton } from "~/components/form-button";
+import { PagePaths } from "~/page-paths.shared";
+
+export function PrebuildArchiveButton(props: { prebuildExternalId: string }): JSX.Element {
+  return (
+    <FormButton
+      action={PagePaths.ArchivePrebuild}
+      method="POST"
+      inputs={{
+        projectExternalId: props.prebuildExternalId,
+      }}
+      buttonProps={{ color: "light", className: "transition-all" }}
+      loadingSpinnerProps={{ color: "gray" }}
+    >
+      <i className="fa-solid fa-box-archive mr-2"></i>
+      <span>Archive</span>
+    </FormButton>
+  );
+}
