a3f70fc19a2476aca06a078c3e05c1da6f83f18e
Hugo Dutka <contact@hugodutka.com>
2023-05-24 11:45:23 +0000

    try fixing race condition

--- app/agent/buildfs.service.ts
+++ app/agent/buildfs.service.ts
@@ -88,20 +88,12 @@ export class BuildfsService {
       buildfsEventId: bigint;
     },
   ): Promise<BuildfsEventFiles> {
-    const projectFile = await db.file.upsert({
-      where: {
-        // eslint-disable-next-line camelcase
-        agentInstanceId_path: {
-          agentInstanceId: args.agentInstanceId,
-          path: args.projectFilePath,
-        },
-      },
-      create: {
-        agentInstanceId: args.agentInstanceId,
-        path: args.projectFilePath,
-      },
-      update: {},
-    });
+    const { id: projectFileIdStr } = await db.$queryRaw<{ id: string }>`
+      INSERT INTO "File" ("agentInstanceId", "path")
+      VALUES (${args.agentInstanceId}, ${args.projectFilePath})
+      ON CONFLICT DO NOTHING
+      RETURNING id`;
+    const projectFileId = BigInt(projectFileIdStr);
     const outputFile = await db.file.create({
       data: {
         agentInstanceId: args.agentInstanceId,
@@ -111,7 +103,7 @@ export class BuildfsService {
     return await db.buildfsEventFiles.create({
       data: {
         buildfsEventId: args.buildfsEventId,
-        projectFileId: projectFile.id,
+        projectFileId,
         outputFileId: outputFile.id,
         agentInstanceId: args.agentInstanceId,
       },
