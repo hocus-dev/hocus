2fb622f3b114a3c4645b155341573c51fb84066a
Hugo Dutka <contact@hugodutka.com>
2023-06-06 10:07:02 +0000

    add a comment why we disable callContextStorage manually

--- app/temporal/client-factory.ts
+++ app/temporal/client-factory.ts
@@ -19,6 +19,16 @@ export const clientFactory = (
       return await fn(client);
     } finally {
       await connection.close();
+      // Connection.close() does not clear the context storage, so we need to do it manually.
+      // This is a bug in the temporal library. If not cleared manually, it causes a memory leak.
+      // If connections are continuously created and closed, it eventually increases cpu usage to 100%.
+      // This is because all AsyncLocalStorage instances are stored in a global object here:
+      // https://github.com/nodejs/node/blob/9f3466bcb646dc2f568569813379be1252753a72/lib/async_hooks.js#L273
+      // The AsyncLocalStorage instances are never removed from the global object, so it grows indefinitely.
+      // Then, every time a promise is created, the global object is iterated over here:
+      // https://github.com/nodejs/node/blob/6fd13beae07fdd8ba7e153c0b0ecc8b440407025/lib/async_hooks.js#L270
+      // Once the global object grows large enough, creating a promise becomes a very expensive operation,
+      // costing 10ms or more of CPU time. This is why CPU usage increases to 100%.
       connection.callContextStorage.disable();
     }
   };
