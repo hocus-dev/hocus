601ec8eb8576764dec533df3df78e4da17e88995
Hugo Dutka <contact@hugodutka.com>
2023-06-07 13:45:55 +0000

    remove redundant eslint comments

--- app/jest/test-setup.ts
+++ app/jest/test-setup.ts
@@ -8,18 +8,15 @@ global.console = console;
 beforeAll(async () => {
   let testStorageDir = process.env.TEST_STORAGE_DIR;
   if (testStorageDir === void 0) {
-    // eslint-disable-next-line no-console
     throw new Error("Please provide TEST_STORAGE_DIR");
   }
   const stateManager = new TestStateManager(testStorageDir);
   await stateManager.connect();
   stateManager.on("error", (err: Error) => {
-    // eslint-disable-next-line no-console
     console.error("State manager terminated due to error", err);
     process.kill(process.pid, "SIGINT");
   });
   stateManager.on("close", () => {
-    // eslint-disable-next-line no-console
     console.error("Connection to state manager terminated");
     process.kill(process.pid, "SIGINT");
   });
