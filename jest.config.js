65c70777f48c676ce1031cfb70577aefcdd18d0c
Grzegorz Uriasz <gorbak25@gmail.com>
2023-06-07 14:46:47 +0000

    Finally fully working stack traces <3

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,17 @@
 require("@remix-run/node").installGlobals();
-/*require("ts-node").register({
-  transpileOnly: true,
-});*/
-require("tsconfig-paths").register();
 
 /** @type {import('jest').Config} */
 var config = {
   transform: {
-    "^.+\\.(t|j)sx?$": [
-      "@swc-node/jest",
-      {
-        target: "ES2020",
-        module: "commonjs",
-        sourcemap: "inline",
-      },
-    ],
+    "^.+\\.(t|j)sx?$": ["@swc/jest"],
   },
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/app/$1",
+    // https://github.com/remix-run/remix/issues/3105
+    "^source-map-support$": "<rootDir>/app/jest/source-map-support.stub.ts",
   },
   testEnvironment: "node",
+  collectCoverage: false,
   //workerThreads: true,
   setupFilesAfterEnv: ["<rootDir>/app/jest/test-setup.ts"],
 };
