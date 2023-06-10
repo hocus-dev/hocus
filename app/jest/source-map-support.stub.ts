78d23655d9f7d2a9c3dbb430cc8e32262d8e9401
Grzegorz Uriasz <gorbak25@gmail.com>
2023-06-10 05:30:06 -0700

    Replace test name with test location (#102)

--- app/jest/source-map-support.stub.ts
+++ app/jest/source-map-support.stub.ts
@@ -1,19 +1,8 @@
+import { discoverCaller } from "~/test-utils/test-environment-builder";
+
 /* eslint-disable no-console */
 export const install = () => {
-  let caller = "[UNKNOWN]";
-  // Play some 4D chess
-  const o = Error.prepareStackTrace;
-  try {
-    Error.prepareStackTrace = (_err, stackTraces) => {
-      const c = stackTraces[1];
-      if (c) caller = `${c.getFileName()}:${c.getLineNumber()}:${c.getColumnNumber()}`;
-    };
-    const a = new Error();
-    void a.stack;
-  } finally {
-    Error.prepareStackTrace = o;
-    console.error(
-      `Warning! Someone tried to install source map support in a jest test. This will cause all stack traces to be malformed! The offender:\n${caller}`,
-    );
-  }
+  console.error(
+    `Warning! Someone tried to install source map support in a jest test. This will cause all stack traces to be malformed! The offender:\n${discoverCaller()}`,
+  );
 };
