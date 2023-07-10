4254b0c459255bd94f8238e9cf8c98a47b210931
Grzegorz Uriasz <gorbak25@gmail.com>
2023-07-10 05:36:03 -0700

    Auto enable SCSI sync scan mode (#112)
    * Auto enable scsi sync mode
    
    * Typo

--- ops/bin/setup-tcmu.sh
+++ ops/bin/setup-tcmu.sh
@@ -13,24 +13,24 @@ fi
 # First check whether we need to load target_core_user
 if ! [ -d /sys/kernel/config/target ] ; then
   if [ -f /proc/modules ] ; then
-    echo \"Trying to load target_core_user\";
+    echo "Trying to load target_core_user";
     modprobe target_core_user;
   fi;
   # Check if loading target_core_user worked
   if ! [ -d /sys/kernel/config/target ] ; then
-    echo \"Kernel module target_core_user is not available\";
+    echo "Kernel module target_core_user is not available";
     exit 1
   fi;
 fi;
 
 # Secondly check whether we need to load tcm_loop
 if ! [ -d /sys/kernel/config/target/loopback/ ] ; then
-  echo \"Trying to load tcm_loop\";
+  echo "Trying to load tcm_loop";
   # Oh perhaps the module is there but not started?
   mkdir /sys/kernel/config/target/loopback/ || true
   # If the directory is not there then the kernel doesn't have tcm_loop
   if ! [ -d /sys/kernel/config/target/loopback/ ] ; then
-    echo \"Kernel module tcm_loop is not available\";
+    echo "Kernel module tcm_loop is not available";
     exit 1
   fi
 fi
@@ -40,6 +40,18 @@ if ! [ -d /sys/bus/scsi/drivers/sd ] ; then
   # TODO: try to load the module
   #       please note that 99% of kernels should have this available
   #       cause otherwise one cannot boot from an hdd ;)
-  echo \"No scsi disk support detected\";
+  echo "No scsi disk support detected";
   exit 1
 fi
+
+# Ensure scsi sync mode is enabled
+if ! [ -f /sys/module/scsi_mod/parameters/scan ] ; then
+  echo "Unable to verify scsi scan mode. /sys/module/scsi_mod/parameters/scan not found";
+  exit 1
+fi;
+
+SCSI_SCAN_MODE=$(cat /sys/module/scsi_mod/parameters/scan)
+if ! [ "$SCSI_SCAN_MODE" = "sync" ] ; then
+  echo "SCSI scan mode not in sync mode, enabling sync mode";
+  echo -n "sync" > /sys/module/scsi_mod/parameters/scan;
+fi;
