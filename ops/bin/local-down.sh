9cc604cd449c7358cec18f533047da792895cd31
Grzegorz Uriasz <gorbak25@gmail.com>
2023-07-11 06:22:50 -0700

    Collect telemetry from local-* scripts (#116)
    * Instrument local-up
    
    * Fix trailing space

--- ops/bin/local-down.sh
+++ ops/bin/local-down.sh
@@ -9,5 +9,7 @@ SCRIPT_DIR="$(dirname "$0")"
 export REPO_DIR="$(realpath "${SCRIPT_DIR}/../..")"
 export HOCUS_RESOURCES_DIR="$(realpath ${REPO_DIR}/../hocus-resources)"
 
+PHOG_EVENT_NAME=local-down eval "$(cat "$REPO_DIR"/ops/bin/phog-telemetry.hook)"
+
 cd "$SCRIPT_DIR"
 "$REPO_DIR/ops/bin/local-cmd.sh" down
