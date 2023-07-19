7e748801e8c9f6945aafd44ca739b2f5e528d1c1
Grzegorz Uriasz <gorbak25@gmail.com>
2023-07-19 06:58:52 -0700

    Disable telemetry when running development builds (#127)

--- ops/bin/worker-in-hocus-entrypoint.sh
+++ ops/bin/worker-in-hocus-entrypoint.sh
@@ -12,6 +12,7 @@ export TEMPORAL_SERVER_URL="temporal:7233"
 export AGENT_DATABASE_URL="postgres://postgres:pass@db:5432/rooms"
 export AGENT_DEV_CREATE_DEVELOPMENT_PROJECTS="true"
 export NODE_ENV="development"
+export TELEMETRY_DISABLED=1
 
 mkdir /kernel/ && ops/bin/download-kernel.sh /kernel/
 yarn run regen
